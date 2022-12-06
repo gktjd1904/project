0d51aafe79ad3aa1e52cb9c7662f464c736d4b66
gktjd1904 <80248494+gktjd1904@users.noreply.github.com>
2022-12-06 13:21:34 +0900

    Create about.js

---
+++ project/project02/js/about.js
@@ -0,0 +1,11 @@
+/* let mainText = document.querySelector("h1");
+
+window.addEventListener('scroll', function() {
+    let value = window.scrollY
+    console.log("scrolly",value);
+
+    if (value > 900) {
+        mainText.style.animation = 
+    
+})
+ */
